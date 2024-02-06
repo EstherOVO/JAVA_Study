6bcb9d8265cc2587f06d9ab056af089846d0aba1
EstherOVO <tmejwjd@gmail.com>
2024-02-06 13:53:26 +0900

    My First Commit Java_How to write comments. Java_Println "Hello World!"

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+// 2024.2.6.
+// Esther Jung
+
+// 실행문 끝에는 반드시 세미콜론(;)을 븉여 실행문이 끝났음을 표시해야 한다.
+// 실행문의 범위는 세미콜론(;) 까지다.
+// 주석 단축기 Ctrl + /
+// 문자열 안에서 혹은 범위 주석 작성 시 /*...*/를 이용하여 주석 처리
+
+
+public class HelloWorld { // 클래스 선언부
+
+    public static void main(String [] args) { // 메소드 선언부
+
+        System.out.println("Hello World!");
+
+
+    }
+}
---
+++ src/Statement.java
@@ -0,0 +1,16 @@
+public class Statement {
+    public static void main(String [] args) {
+
+//      Variable
+//      int x (Integer Variable x) : x라는 변수(정수)를 선언
+        int x = 1; // "x에 1을 저장하라."는 의미
+        int y = 2; // "y를 선언하고 2를 저장하라."는 의미
+        int result = x + y; // "result를 선언하고 x와 y를 더해서 저장하라."는 의미
+        System.out.println(result); // "출력하라."는 의미
+
+
+
+
+
+    }
+}
