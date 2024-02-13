68c8d5fbd8fcc090e4e2968e827c5358c3659c2d
EstherOVO <tmejwjd@gmail.com>
2024-02-13 09:01:50 +0900

    Java Condition Examples

--- src/Condition/Ex01.java
+++ src/Condition/Ex01.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Ex01 {
     public static void main(String[] args) {
 
-//        환율 계산하기
-//        달러 환율이 1327원이다.
-//        달러를 0원 입금받으면 "환전할 금액이 없습니다."
-//        양수일 때 "환전 금액은 00000원 입니다."
-//        음수일 때 "잘못 입력하셨습니다."
+/*
+        환율 계산하기
+        달러 환율이 1327원이다.
+        달러를 0원 입금받으면 "환전할 금액이 없습니다."
+        양수일 때 "환전 금액은 00000원 입니다."
+        음수일 때 "잘못 입력하셨습니다."
+ */
 
         Scanner scanner = new Scanner(System.in);
 
