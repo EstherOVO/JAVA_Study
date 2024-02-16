3e6f4441026639cba2115f0cab7160e05b7c0cd2
EstherOVO <tmejwjd@gmail.com>
2024-02-16 11:59:44 +0900

    Java Reference

---
+++ src/Reference/Data.java
@@ -0,0 +1,8 @@
+package Reference;
+
+public class Data {
+
+//  클래스에서 필드를 생성 시 값을 초기화하지 않으면 초기값이 들어간다.
+    int value1;         // 일반적으로 초기화하지 않음
+    int value2 = 10;    // 초기화를 하는 경우
+}
---
+++ src/Reference/Ref03.java
@@ -0,0 +1,20 @@
+package Reference;
+
+public class Ref03 {
+    public static void main(String[] args) {
+
+        Data data1 = new Data();    // 데이터 생성하여 참조값을 data1에 저장한다. data = 0x10
+        data1.value1 = 15;
+        Data data2 = data1; // 객체의 위치를 가리키는 참조값을 복사한다. data2 = 0x10
+
+        int a = 10, b = 20;
+        int sum = a + b;    // 기본 타입은 연산이 가능하다.
+
+//      int dataSum = data1 + data2; 참조 타입은 기본적으로 연산이 불가능하다. (문자열의 연산(글자가 이어지는 것)은 제외)
+        int dataSum = data1.value1 + data2.value2;  // 참조 타입의 기본 타입 필드에 접근하면 연산 가능하다.
+
+//      String 문자열은 특별한 타입으로 문자열 연결 연산 가능하다.
+//      예시 String a = "1" / String b = "2" → a + b → 출력 : 12
+
+    }
+}
