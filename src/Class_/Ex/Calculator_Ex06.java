bc171e3b567480b69d3cb4c2de00a160e14d7fdf
EstherOVO <tmejwjd@gmail.com>
2024-02-19 11:03:05 +0900

    Java Class Exercises

---
+++ src/Class_/Ex/Calculator_Ex06.java
@@ -0,0 +1,13 @@
+package Class_.Ex;
+
+import java.util.Scanner;
+
+public class Calculator_Ex06 {
+    public static void main(String[] args) {
+
+//      객체지향 코드로 변경
+        Calculator calculator = new Calculator();
+
+        calculator.getAnswer();
+    }
+}
