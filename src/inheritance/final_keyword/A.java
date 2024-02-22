146f736a2c96eeb8423915e8b3893c25989f7931
EstherOVO <tmejwjd@gmail.com>
2024-02-22 14:16:28 +0900

    Java Inheritance

---
+++ src/inheritance/final_keyword/A.java
@@ -0,0 +1,15 @@
+package inheritance.final_keyword;
+
+// 클래스에 선언된 final의 경우 더 이상 상속받을 수 없음을 나타낸다.
+// final public class A {
+
+public class A {
+
+//  메소드에 선언된 final은 더 이상 재정의(Override) 될 수 없음을 나타낸다.
+// final public void method() {
+//    System.out.println("메소드");
+
+    public void method() {
+        System.out.println("메소드");
+    }
+}
