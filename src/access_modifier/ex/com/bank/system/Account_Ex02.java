4b9ebde3cd854eac0044f8d16df4e0b68295fc19
EstherOVO <tmejwjd@gmail.com>
2024-02-23 09:54:34 +0900

    Java Access Modifier Exercises

--- src/access_modifier/ex/com/bank/system/Account_Ex02.java
+++ src/access_modifier/ex/com/bank/system/Account_Ex02.java
@@ -1,15 +1,15 @@
 package access_modifier.ex.com.bank.system;
 
-public class Account_Ex {
+public class Account_Ex02 {
     public static void main(String[] args) {
 
 /*
-        패키지명: com.bank.system
-        클래스명: Account
-        필드:
+        패키지명 : com.bank.system
+        클래스명 : Account
+        필드 :
         balance; → 계좌의 잔액 / 외부 접근 금지
-        메서드:
-        getBalance() → 잔액 조회 / 기능: 현재 잔액을 반환
+        메서드 :
+        getBalance() → 잔액 조회 / 기능 : 현재 잔액을 반환
         deposit(double amount) → 입금 / 기능 : 잔액에 금액을 추가
         withdraw(double amount) → 출금 / 잔액보다 많은 금액을 출금하려고 하면 출금을 거부해야 함
         기능: 잔액이 출금할 금액보다 크거나 같을 경우에만 출금 처리. 그렇지 않으면 경고 메시지 출력
@@ -21,12 +21,13 @@ public class Account_Ex {
 
         실행 결과
         ===
-        초기 잔액: 0.0
-        입금 후 잔액: 100.0
-        출금 후 잔액: 50.0
-        출금 시도: 100.0
+        초기 잔액 : 0.0
+        입금 후 잔액 : 100.0
+        출금 후 잔액 : 50.0
+        출금 시도 : 100.0
         출금 실패. 잔액 부족
 */
+
         Account account = new Account();
 
         System.out.println("= 실행결과 =");
