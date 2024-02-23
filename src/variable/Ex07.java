a80a0f0ca38c7ba2491a7321e9785e4792231543
EstherOVO <tmejwjd@gmail.com>
2024-02-23 09:21:38 +0900

    Java Variable Exercises

--- src/variable/Ex07.java
+++ src/variable/Ex07.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class Ex07 {
     public static void main(String[] args) {
 
-/*      Scanner를 이용해서 변수에 입력을 받고 출력하는 코드를 작성하라.
+/*
+        Scanner를 이용해서 변수에 입력을 받고 출력하는 코드를 작성하라.
         1. 이름: ________ (ENTER)
         2. 주민번호앞자리: _______ (ENTER)
         3. 전화번호: ______ (ENTER)
-*/
 
-        /* Scanner scanner = new Scanner(System.in);   // 키보드에 입력된 내용을 문자열로 얻기
+        Scanner scanner = new Scanner(System.in);   // 키보드에 입력된 내용을 문자열로 얻기
         String line = scanner.nextLine();
 
-        System.out.println(line); */
+        System.out.println(line);
+*/
 
         Scanner sc = new Scanner(System.in);
 
