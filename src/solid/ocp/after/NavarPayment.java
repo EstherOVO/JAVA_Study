994431274eff2610876ac9554b309d2a87127c52
EstherOVO <tmejwjd@gmail.com>
2024-02-27 14:49:17 +0900

    Java OOP SOLID

--- src/solid/ocp/after/NavarPayment.java
+++ src/solid/ocp/after/NavarPayment.java
@@ -2,6 +2,7 @@ package solid.ocp.after;
 
 public class NavarPayment implements Payment {
 
+//  새로운 결제 방법 확장에는 열려있다.
     @Override
     public void processPayment() {
         System.out.println("네이바페이 결제를 진행합니다.");
