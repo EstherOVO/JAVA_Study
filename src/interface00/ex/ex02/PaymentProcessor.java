bc85736850f98e5875c4f4a50f1309f927db7dd2
EstherOVO <tmejwjd@gmail.com>
2024-02-28 10:34:28 +0900

    Java Interface Exercises

--- src/interface00/ex/ex02/PaymentProcessor.java
+++ src/interface00/ex/ex02/PaymentProcessor.java
@@ -2,8 +2,6 @@ package interface00.ex.ex02;
 
 public class PaymentProcessor {
 
-    private Payment payment;
-
     public void payment(Payment payment, double amount) {
         payment.pay(amount);
     }
