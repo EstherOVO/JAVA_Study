66b28240b914cdd3cb404b90e1d3414c8edf1892
EstherOVO <tmejwjd@gmail.com>
2024-03-11 09:04:13 +0900

    Java Library Exercises

--- src/library/util/Ex04.java
+++ src/library/util/Ex04.java
@@ -1,11 +1,7 @@
 package library.util;
 
-import java.time.Duration;
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Ex04 {
@@ -32,30 +28,23 @@ public class Ex04 {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("생년월일을 입력해 주세요(YYYY-MM-DD일 형식) : ");
-        String birth = scanner.nextLine();
+        System.out.print("생년월일을 입력해 주세요 : ");
+        String input = scanner.nextLine();
 
-        String[] strings = birth.split("-");
-
-        int month = Integer.parseInt(strings[1]);
-        int day = Integer.parseInt(strings[2]);
+        String[] birth = input.split("-");
+        int month = Integer.parseInt(birth[1]);
+        int day = Integer.parseInt(birth[2]);
 
         LocalDate now = LocalDate.now();
         LocalDate thisBirthDay = LocalDate.of(now.getYear(), month, day);
         LocalDate nextBirthDay = LocalDate.of(now.getYear() + 1, month, day);
 
-        if (LocalDate.now().isBefore(thisBirthDay)) {
-            Period between1 = Period.between(now, thisBirthDay);
-            long onlyDays1 = now.until(thisBirthDay, ChronoUnit.DAYS);
-            int days1 = between1.getDays();
-            int months1 = between1.getMonths();
-            System.out.println(String.format("생일까지 남은 기간은 %d개월 %d일입니다! (총 %d일)", months1, days1, onlyDays1));
-        } else {
-            Period between2 = Period.between(now, nextBirthDay);
-            long onlyDays2 = now.until(nextBirthDay, ChronoUnit.DAYS);
-            int days2 = between2.getDays();
-            int months2 = between2.getMonths();
-            System.out.println(String.format("다음 생일까지 남은 기간은 %d개월 %d일입니다! (총 %d일)", months2, days2, onlyDays2));
+        if (now.isBefore(thisBirthDay)) {
+            long nowToThisBirthDay = now.until(thisBirthDay, ChronoUnit.DAYS);
+            System.out.println("오늘부터 생일까지 %d일 남았습니다!".formatted(nowToThisBirthDay));
+        } else if (now.isAfter(thisBirthDay)) {
+            long nowToNextBirthDay = now.until(nextBirthDay, ChronoUnit.DAYS);
+            System.out.println("생일이 지났네요. 오늘부터 다음 생일까지 %d일 남았습니다!".formatted(nowToNextBirthDay));
         }
     }
 }
