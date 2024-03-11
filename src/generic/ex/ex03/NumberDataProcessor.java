76c9ebb7422368c55573014bcb33f7c041f809da
EstherOVO <tmejwjd@gmail.com>
2024-03-11 12:52:47 +0900

    Java Generic Exercises

--- src/generic/ex/ex03/NumberDataProcessor.java
+++ src/generic/ex/ex03/NumberDataProcessor.java
@@ -1,8 +1,9 @@
 package generic.ex.ex03;
 
 public class NumberDataProcessor implements DataProcessor<Integer> {
+
     @Override
     public Integer processData(Integer integer) {
-        return integer;
+        return integer * 2;
     }
 }
