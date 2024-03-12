9320cffe87b4e5c0181804db7f1cb8296e38b04e
EstherOVO <tmejwjd@gmail.com>
2024-03-12 11:16:21 +0900

    Java Collection

--- src/collection/list/LinkedListSample.java
+++ src/collection/list/LinkedListSample.java
@@ -7,6 +7,14 @@ import java.util.List;
 public class LinkedListSample {
     public static void main(String[] args) {
 
+/*
+        ArrayList vs LinkedList
+        ArrayList : 인덱스를 통한 빠른 접근이 필요한 상황
+        - 조회가 많고, 추가나 삭제가 적은 경우 유리하게 사용
+        LinkedList : 데이터의 추가 및 삭제가 빈번하게 발생하는 경우 사용
+        - 특히, 대용량 데이터의 삽입, 삭제가 이루어지는 경우 유리하게 사용한다.
+*/
+
         List<String> arrayList = new ArrayList<>();
         List<String> linkedList = new LinkedList<>();
 
