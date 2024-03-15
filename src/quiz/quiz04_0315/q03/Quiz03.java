9fc7db8b80ebef1cc627b83a79e42cc09cdbcebe
EstherOVO <tmejwjd@gmail.com>
2024-03-15 15:13:27 +0900

    Java Quiz

--- src/quiz/quiz04_0315/q03/Quiz03.java
+++ src/quiz/quiz04_0315/q03/Quiz03.java
@@ -18,8 +18,6 @@ public class Quiz03 {
         int days = period.getDays();
         long until = LocalDate.now().until(worldCup, ChronoUnit.DAYS);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 M월 dd일");
-
         System.out.println("= 월드컵 D-DAY =");
         System.out.println("다음 월드컵까지 %d년 %d개월 %d일(총 %d일) 남았습니다!".formatted(years, months, days, until));
     }
