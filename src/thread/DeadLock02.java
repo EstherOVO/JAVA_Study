1646d86aa253dc08375a149bc2f6ffc345ae0375
EstherOVO <tmejwjd@gmail.com>
2024-03-25 15:33:45 +0900

    Java Thread

---
+++ src/thread/DeadLock02.java
@@ -0,0 +1,84 @@
+package thread;
+
+public class DeadLock02 {
+//  식사하는 철학자들 문제
+
+    public static void main(String[] args) {
+
+//      포크 5개
+        Fork[] forks = new Fork[5];
+
+//      포크 초기화
+        for (int i = 0; i < forks.length; i++) {
+            forks[i] = new Fork(i);
+        }
+
+//      철학자 5명
+        Philosopher[] philosophers = new Philosopher[5];
+
+//      철학자 초기화(원형 테이블)
+        for (int i = 0; i < philosophers.length; i++) {
+            Fork leftFork = forks[i];
+            Fork rightFork = forks[(i + 1) % forks.length];
+            philosophers[i] = new Philosopher(leftFork, rightFork);
+            new Thread(philosophers[i], "철학자 " + (i + 1)).start();
+        }
+    }
+}
+
+class Fork {
+
+//  포크는 ID를 가짐
+    int id;
+
+    public Fork(int id) {
+        this.id = id;
+    }
+}
+
+class Philosopher implements Runnable {
+
+    Fork leftFork;
+    Fork rightFork;
+
+    public Philosopher(Fork leftFork, Fork rightFork) {
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+
+        while (true) {
+
+//      1. 일정 시간 생각한다.
+            System.out.println(name + "이(가) 생각한다.");
+
+//      2. 왼쪽 포크가 사용 가능할 때까지 대기 → 사용 가능하면 집어든다.
+            synchronized (leftFork) {
+                System.out.println(name + "이(가) 왼쪽 포크를 든다.");
+
+
+//      3. 오른쪽 포크가 사용 가능할 때가지 대기 → 사용 가능하면 집어든다.
+                synchronized (rightFork) {
+                    System.out.println(name + "이(가) 오른쪽 포크를 든다.");
+
+
+//      4. 양쪽 포크를 잡으면 식사한다.
+                    System.out.println(name + "이(가) 식사를 한다.");
+                }
+
+//      5. 오른쪽 포크를 내려놓는다.
+                System.out.println(name + "이(가) 오른쪽 포크를 내려놓는다.");
+            }
+
+//      6. 왼쪽 포크를 내려놓는다.
+            System.out.println(name + "이(가) 왼쪽 포크를 내려놓는다.");
+        }
+
+//      7. 처음으로 돌아간다.
+    }
+}
+
+
