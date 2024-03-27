07d70a6be3e01f4e2f74436b85418343a0f8b166
EstherOVO <tmejwjd@gmail.com>
2024-03-27 11:41:16 +0900

    Java I/O Stream

--- src/io_stream/serial/Person.java
+++ src/io_stream/serial/Person.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class Person implements Serializable {
 
+//  serialVersionUID : 직렬화하여 주고받을 해당 클래스의 버전
+//  객체를 보낼 쪽과 받을 쪽의 명시된 클래스의 내용이 다를 경우를 대비
+//  직접 지정하지 않으면 자동으로 생성
+    private static final long serialVersionUID = 1;
     private String name;
     private int age;
     private double height;
@@ -27,6 +31,6 @@ public class Person implements Serializable {
                 ", height = " + height +
                 ", married = " + married +
                 ", weight = " + weight +
-                ", career = " + career;
+                ", career = [" + career +"]";
     }
 }
--- src/io_stream/serial/Serial02.java
+++ src/io_stream/serial/Serial02.java
@@ -1,16 +1,22 @@
 package io_stream.serial;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
+import java.io.*;
 
 public class Serial02 {
     public static void main(String[] args) {
 
         String filePath = "src/io_stream/serial/person.ser";
 
-        FileInputStream fis = new FileInputStream(filePath);
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        ObjectInputStream ois = new ObjectInputStream(bis);
+
+        try (FileInputStream fis = new FileInputStream(filePath);
+             BufferedInputStream bis = new BufferedInputStream(fis);
+             ObjectInputStream ois = new ObjectInputStream(bis);) {
+
+            Person person = (Person) ois.readObject();
+
+            System.out.println(person);
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
