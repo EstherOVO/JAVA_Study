93f47a0d344beefb3cb731fed339bc00eb987ae1
EstherOVO <tmejwjd@gmail.com>
2024-03-29 09:07:55 +0900

    Java Network Exercises

--- src/network/ex/Ex02.java
+++ src/network/ex/Ex02.java
@@ -4,11 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
