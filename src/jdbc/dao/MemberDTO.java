764ea8f1bd7598aaf7a5741c41b27eee8c8e48a9
EstherOVO <tmejwjd@gmail.com>
2024-04-25 09:58:44 +0900

    Java JDBC(Java Database Connectivity)

---
+++ src/jdbc/dao/MemberDTO.java
@@ -0,0 +1,40 @@
+package jdbc.dao;
+
+public class MemberDTO {
+
+//  데이터를 전달한 컬럼(DB의 컬럼과 일치)
+    private String id;
+    private String name;
+    private String email;
+
+    public MemberDTO(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    //  Getter, Setter
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
