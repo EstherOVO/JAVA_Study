e7e404eaabe24eec0efb3bde4a4272abc59e5f53
EstherOVO <tmejwjd@gmail.com>
2024-04-25 11:34:01 +0900

    Java JDBC(Java Database Connectivity)

---
+++ src/jdbc/dao/MemberDAOImpl.java
@@ -0,0 +1,98 @@
+package jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MemberDAOImpl implements MemberDAO {
+
+    Connection conn;
+
+//  DAO 생성과 동시에 접근 객체 생성
+    public MemberDAOImpl() {
+        this.conn = DatabaseUtil.getConnection();
+    }
+
+    @Override
+    public void insert(MemberDTO member) {
+
+        String sql = "INSERT INTO members (id, name, email) VALUES (?, ?, ?)";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql);) {
+
+            pstmt.setString(1, member.getId());
+            pstmt.setString(2, member.getName());
+            pstmt.setString(3, member.getEmail());
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public MemberDTO getMemberById(String id) {
+
+        String sql = "SELECT id, name, email FROM members WHERE id = ?";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql);) {
+
+            pstmt.setString(1, id);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+
+                return new MemberDTO(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        rs.getString("email"));
+            }
+
+            rs.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
+    public void update(MemberDTO member) {
+
+        String sql = "UPDATE members SET id = ?, name = ?, email = ? WHERE id = ?";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, member.getId());
+            pstmt.setString(2, member.getName());
+            pstmt.setString(3, member.getEmail());
+            pstmt.setString(4, member.getId());
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(String id) {
+
+        String sql = "DELETE FROM members WHERE id = ?";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, id);
+
+            pstmt.executeUpdate();
+
+            conn.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/jdbc/dao/MemberMain.java
@@ -0,0 +1,115 @@
+package jdbc.dao;
+
+import java.util.Scanner;
+
+public class MemberMain {
+
+
+//  메인 실행 계층에서는 데이터 접근과 관련된 모든 코드를 DAO에게 위임
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+//      데이터베이스 접근 객체 생성
+        MemberDAO memberDAO = new MemberDAOImpl();
+
+        System.out.print("1. Insert | 2. Select | 3. Update | 4. Delete > ");
+        String menu = scanner.nextLine();
+
+        switch (menu) {
+            case "1" -> insert(memberDAO);
+            case "2" -> select(memberDAO);
+            case "3" -> update(memberDAO);
+            case "4" -> delete(memberDAO);
+        }
+    }
+
+    private static void insert(MemberDAO memberDAO) {
+
+        Scanner scanner = new Scanner(System.in);
+
+//      회원 가입
+        System.out.println("[회원 생성]");
+        System.out.print("ID : ");
+        String id = scanner.nextLine();
+        System.out.print("Name : ");
+        String name = scanner.nextLine();
+        System.out.print("Email : ");
+        String email = scanner.nextLine();
+
+//      DTO 객체 생성
+        MemberDTO member = new MemberDTO(id, name, email);
+
+//      데이터 접근은 DAO에게 위임
+        memberDAO.insert(member);
+    }
+
+    private static void select(MemberDAO memberDAO) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("[회원 조회]");
+        System.out.print("ID : ");
+        String id = scanner.nextLine();
+
+        MemberDTO member = memberDAO.getMemberById(id);
+
+        System.out.println("ID : " + member.getId());
+        System.out.println("Name : " + member.getName());
+        System.out.println("Email : " + member.getEmail());
+    }
+
+    private static void update(MemberDAO memberDAO) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("[회원 조회]");
+        System.out.print("ID : ");
+        String id = scanner.nextLine();
+
+        MemberDTO member = memberDAO.getMemberById(id);
+
+        System.out.println("ID : " + member.getId());
+        System.out.println("Name : " + member.getName());
+        System.out.println("Email : " + member.getEmail());
+
+        if (member != null) {
+
+            System.out.println("[회원 정보 수정]");
+            System.out.print("Name : ");
+            String name = scanner.nextLine();
+            if (!name.isEmpty()) member.setName(name);
+
+            System.out.print("Email : ");
+            String email = scanner.nextLine();
+            if (!email.isEmpty()) member.setEmail(email);
+
+            memberDAO.update(member);
+
+            System.out.println("회원 정보가 변경되었습니다.");
+
+        } else {
+            System.out.println("존재하지 않는 회원입니다.");
+        }
+    }
+
+    private static void delete(MemberDAO memberDAO) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("[회원 삭제]");
+        System.out.print("ID : ");
+        String id = scanner.nextLine();
+
+        MemberDTO member = memberDAO.getMemberById(id);
+
+        if (member != null) {
+
+            memberDAO.delete(id);
+            System.out.println("회원 정보가 삭제되었습니다.");
+
+        } else {
+            System.out.println("존재하지 않는 회원입니다.");
+        }
+    }
+}
